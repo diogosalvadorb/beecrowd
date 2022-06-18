a52ec81a68202fa27705ddf4c69321514c5e9a55
diogosalvadorb <105182603+diogosalvadorb@users.noreply.github.com>
2022-06-17 22:44:34 -0300

    1013, 1015, 1017

--- src/1013/1013/Program.cs
+++ src/1013/1013/Program.cs
@@ -6,7 +6,18 @@ namespace _1013
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x, y, z, maiorxy, maior;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);
+            z = int.Parse(valores[2]);
+
+            maiorxy = (x + y + Math.Abs(x - y)) / 2;
+
+            maior = (maiorxy + z + Math.Abs(maiorxy - z)) / 2;
+
+            Console.WriteLine($"{maior} eh o maior");
         }
     }
 }
